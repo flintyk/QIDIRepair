db29fc6381fd1e29076aab810ef806d5c229d64c
Евгений <flintyk@gmail.com>
2024-11-14 17:46:40 +0300

    Fixes mistakes of updates path

--- scripts/m3update4312.sh
+++ scripts/m3update4312.sh
@@ -2,7 +2,7 @@
 #name:v4.1.12
 #dir:X-Max 3/Update
 #description:Update v4.3.12
-sudo sh -c 'wget "https://www.qidi-russia.ru/3series/updates/smart/4.1.12/mks.deb" -O /tmp/mks.deb'
+sudo sh -c 'wget "https://www.qidi-russia.ru/3series/updates/max/4.1.12/mks.deb" -O /tmp/mks.deb'
 sudo sh -c "systemctl stop makerbase-client && systemctl stop klipper && systemctl stop moonraker"
 sudo sh -c "dpkg -i /tmp/mks.deb"
 sudo sh -c "systemctl start klipper && systemctl start moonraker && systemctl start makerbase-client"
--- scripts/p3update4213.sh
+++ scripts/p3update4213.sh
@@ -2,7 +2,7 @@
 #name:v4.2.13
 #dir:X-Plus 3/Update
 #description:Update v4.2.13
-sudo sh -c 'wget "https://www.qidi-russia.ru/3series/updates/smart/4.2.13/mks.deb" -O /tmp/mks.deb'
+sudo sh -c 'wget "https://www.qidi-russia.ru/3series/updates/max/4.2.13/mks.deb" -O /tmp/mks.deb'
 sudo sh -c "systemctl stop makerbase-client && systemctl stop klipper && systemctl stop moonraker"
 sudo sh -c "dpkg -i /tmp/mks.deb"
 sudo sh -c "systemctl start klipper && systemctl start moonraker && systemctl start makerbase-client"
