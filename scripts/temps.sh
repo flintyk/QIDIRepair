e4b049bc6fcf7703e3d2f3ef85cb94833a12d1ae
Евгений <flintyk@gmail.com>
2025-01-03 23:55:05 +0300

    Temps for Q1 Pro fix

--- scripts/temps.sh
+++ scripts/temps.sh
@@ -2,7 +2,17 @@
 #name:Turn on temperatures
 #dir:General
 #description:Turn on temperatures
+printer=`(grep -qxF '[mcu U_1]' /home/mks/klipper_config/printer.cfg&&echo "q1") || echo "x3"`
+case "q1" in
 
-grep -qxF '[temperature_sensor Host]' /home/mks/klipper_config/printer.cfg || sed -i '/^\[extruder\]/i [temperature_sensor Host]\nsensor_type: temperature_host\nmin_temp: 10\nmax_temp: 100\n\n[temperature_sensor Mcu_temp]\nsensor_type: temperature_mcu\nmin_temp: 0\nmax_temp: 100\n\n[temperature_sensor Toolhead]\nsensor_type: temperature_mcu\nsensor_mcu: MKS_THR\nmin_temp: 0\nmax_temp: 100\n' /home/mks/klipper_config/printer.cfg
-sudo systemctl restart klipper
+  "$printer"*)
+    grep -qxF '[temperature_sensor Host]' /home/mks/klipper_config/printer.cfg || sed -i '/^\[extruder\]/i [temperature_sensor Host]\nsensor_type: temperature_host\nmin_temp: 10\nmax_temp: 100\n\n[temperature_sensor Toolhead]\nsensor_type: temperature_mcu\nmin_temp: 0\nmax_temp: 100\n\n[temperature_sensor Mcu_temp]\nsensor_type: temperature_mcu\nsensor_mcu: U_1\nmin_temp: 0\nmax_temp: 100\n' /home/mks/klipper_config/printer.cfg
+    ;;
+
+    *)
+    grep -qxF '[temperature_sensor Host]' /home/mks/klipper_config/printer.cfg || sed -i '/^\[extruder\]/i [temperature_sensor Host]\nsensor_type: temperature_host\nmin_temp: 10\nmax_temp: 100\n\n[temperature_sensor Mcu_temp]\nsensor_type: temperature_mcu\nmin_temp: 0\nmax_temp: 100\n\n[temperature_sensor Toolhead]\nsensor_type: temperature_mcu\nsensor_mcu: MKS_THR\nmin_temp: 0\nmax_temp: 100\n' /home/mks/klipper_config/printer.cfg
+    ;;
+esac
+
+sudo sh -c 'systemctl restart klipper'
 exit 0
