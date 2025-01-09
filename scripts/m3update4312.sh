736332a84a6f0fd0b6dcdce08fb36c6aad3ef061
Евгений <flintyk@gmail.com>
2025-01-09 20:23:18 +0300

    dpkg force overwite update

--- scripts/m3update4312.sh
+++ scripts/m3update4312.sh
@@ -4,7 +4,7 @@
 #description:Update v4.3.12
 sudo sh -c 'wget "https://www.qidi-russia.ru/3series/updates/max/4.3.12/mks.deb" -O /tmp/mks.deb'
 sudo sh -c "systemctl stop makerbase-client && systemctl stop klipper && systemctl stop moonraker"
-sudo sh -c "dpkg -i /tmp/mks.deb"
+sudo sh -c "dpkg -i --force-overwrite /tmp/mks.deb"
 sudo sh -c "systemctl start klipper && systemctl start moonraker && systemctl start makerbase-client"
 
 exit 0
