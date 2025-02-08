6ab496f4f91451b3e3fa03b3b608f8949c026c97
Евгений <flintyk@gmail.com>
2025-02-08 13:04:53 +0300

    Fix 4212 file path

--- scripts/p3screen4212.sh
+++ scripts/p3screen4212.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#name:v4.3.12
+#name:v4.2.12
 #dir:X-Plus 3/Flash display firmware
 #description:Flash display v4.3.12
 sudo sh -c 'wget "https://www.qidi-russia.ru/3series/firmwares/screen/plus/4.2.12/800_480.tft" -O /root/800_480.tft'
