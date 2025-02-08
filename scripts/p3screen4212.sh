ba4a5fa4ab9ead6d07d220372d87b83535759f7e
Евгений <flintyk@gmail.com>
2025-02-08 13:03:05 +0300

    Fix paths for P3 screen firmwares

--- scripts/p3screen4212.sh
+++ scripts/p3screen4212.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#name:v4.3.15
+#name:v4.3.12
 #dir:X-Plus 3/Flash display firmware
-#description:Flash display v4.3.15
-sudo sh -c 'wget "https://www.qidi-russia.ru/3series/firmwares/screen/plus/4.3.15/800_480.tft" -O /root/800_480.tft'
+#description:Flash display v4.3.12
+sudo sh -c 'wget "https://www.qidi-russia.ru/3series/firmwares/screen/plus/4.2.12/800_480.tft" -O /root/800_480.tft'
 sudo sh -c "systemctl restart makerbase-client"
 echo "Wait for the operation to complete. The progress is displayed on the printer screen."
 
--- scripts/p3screen4213.sh
+++ scripts/p3screen4213.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#name:v4.3.12
+#name:v4.2.13
 #dir:X-Plus 3/Flash display firmware
-#description:Flash display v4.3.12
-sudo sh -c 'wget "https://www.qidi-russia.ru/3series/firmwares/screen/plus/4.3.12/800_480.tft" -O /root/800_480.tft'
+#description:Flash display v4.2.13
+sudo sh -c 'wget "https://www.qidi-russia.ru/3series/firmwares/screen/plus/4.2.13/800_480.tft" -O /root/800_480.tft'
 sudo sh -c "systemctl restart makerbase-client"
 echo "Wait for the operation to complete. The progress is displayed on the printer screen."
 
